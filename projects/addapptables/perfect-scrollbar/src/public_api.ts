16ef10409dd9604d538405385c46f80e29505c69
mateoguerra <mateo.guerra@ubiquotelemedicina.com>
2019-01-25 12:58:19 -0500

    changed readme and exports

--- projects/addapptables/perfect-scrollbar/src/public_api.ts
+++ projects/addapptables/perfect-scrollbar/src/public_api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of perfect-scrollbar
  */
 
-export { PerfectScrollbarDirective } from './lib/perfect-scrollbar.directive';
-export { DynamicDirective } from './lib/dynamic.directive';
-export { AddapptablePerfectScrollbarModule } from './lib/addapptable-perfect-scrollbar.module';
+export * from './lib/perfect-scrollbar.directive';
+export * from './lib/dynamic.directive';
+export * from './lib/addapptable-perfect-scrollbar.module';
